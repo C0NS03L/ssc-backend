6eba24d6918a666217cf640035ec4b6b1b42b480
C0NS03L <58496201+C0NS03L@users.noreply.github.com>
2024-07-04 18:40:35 +0700

    feat: add new response type

--- src/main/java/com/moneytrackerbackend/dto/AuthenticationResponse.java
+++ src/main/java/com/moneytrackerbackend/dto/AuthenticationResponse.java
@@ -2,17 +2,42 @@ package com.moneytrackerbackend.dto;
 
 public class AuthenticationResponse {
 
-    private String token;
+    private String jwt;
+    private String username;
+    private Long userId;
 
-    public AuthenticationResponse(String token) {
-        this.token = token;
+    // Constructor
+    public AuthenticationResponse(String jwt, String username, Long userId) {
+        this.jwt = jwt;
+        this.username = username;
+        this.userId = userId;
     }
 
-    public String getToken() {
-        return token;
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
     }
 
-    public void setToken(String token) {
-        this.token = token;
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String token) {
+        this.jwt = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 }
